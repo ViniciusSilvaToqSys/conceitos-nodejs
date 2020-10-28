11f6e504eb2262202ae6e69e41bf5b487f3d1445
Vinicius Silva <vinicius@toqsys.com.br>
2020-10-28 11:05:40 -0300

    Projeto com 2 falhas no teste

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+//const { uuid, isUuid } = require('uuidv4');
+const { v4: uuid, validate: isUuid } = require('uuid');
 const cors = require("cors");
+
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -10,24 +13,85 @@ app.use(cors());
 
 const repositories = [];
 
+function validarUuid (request, response, next){
+  const {id} = request.params;
+
+  if (isUuid(id)){
+    next();
+  } else {
+    return response.status(400).json({error: "Invalid repository ID."})
+   }
+}
+
+app.use('/repositories/:id', validarUuid);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => respository.id == id);
+  
+  if (repositoryIndex < 0){
+    return response.status(400).json({error:"repository not found"})
+  }
+
+  repository = {
+    ...repositories[repositoryIndex],
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  }
+    
+  repositories[repositoryIndex] = repository;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex >= 0){
+    repositories.splice(repositoryIndex, 1);
+  } else {
+    return response.status(400).json({error: "Repository not exists."})
+  }
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex == -1){
+    return response.status(400).json({error: "Repository not exists."});
+  }
+
+  repositories[repositoryIndex].likes += 1;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
